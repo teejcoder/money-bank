d681694fa7fcd484ca2439eb48d984efb5ea0ba0
Teej <96551689+teejcoder@users.noreply.github.com>
2023-09-08 19:04:59 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import GoogleAuth from './components/GoogleAuth';
+import { supabase } from './supabaseClient';
+
+const App = () => {
+
+return (
+  <div className='flex justify-center items-center h-screen bg-slate-800 text-white font-montserrat'>
+    <p className='text-3xl'>
+      hi there, this is <b>Money Bank</b> welcome to your financial future
+    </p>
+
+    <GoogleAuth />
+  </div>
+)
+
+}
+
+export default App
