4fa209be931a4d32ebdc8602aec0ca9bef6fd00f
Teej <96551689+teejcoder@users.noreply.github.com>
2023-09-09 10:35:29 +1000

    latest changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
 return (
   <div className='flex justify-center items-center h-screen bg-slate-800 text-white font-montserrat'>
     <p className='text-3xl'>
-      hi there, this is <b>Money Bank</b> welcome to your financial future
+      hi there, this is <b>Money Bank</b> welcome to your financial future!
     </p>
 
     <GoogleAuth />
