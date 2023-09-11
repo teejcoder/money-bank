457faf6aa0d9714a6049c8e85f3bcf605a1b47a1
Teej <96551689+teejcoder@users.noreply.github.com>
2023-09-11 15:45:34 +1000

    added routing for login / signup

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div id="error-page" className="h-screen flex items-center justify-center flex-col">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
