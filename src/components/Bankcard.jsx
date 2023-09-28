0e1aa218cff75b2ddd8cd4241c471324a03b13f2
Teej <96551689+teejcoder@users.noreply.github.com>
2023-09-28 14:58:10 +1000

    Basiq API integration

--- src/components/Bankcard.jsx
+++ src/components/Bankcard.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import UseFetch from '../hook/UseFetch';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+var qs = require('qs');
+var data = qs.stringify({
+  'scope': 'SERVER_ACCESS' 
+})
 
 const Bankcard = () => {
 
+  useEffect(() => {
+      const displayData = async () => {
+        var config = {
+        method: 'post',
+        url: 'https://au-api.basiq.io/token',
+        headers: { 
+          Authorization: 'Basic MzA4NDMyZTAtNjgzOS00ZGU0LWJkZDEtOWVhMzc2ZWUyZDJhOmQ4OTE0MjZhLWMzMmYtNDc1Ni04OTIyLWQyNjM2ZmEzODZjNw==', 
+          'Content-Type': 'application/x-www-form-urlencoded', 
+          'basiq-version': '3.0'
+        },
+        data : data
+    };
+    axios(config)
+      .then(() => {
+      console.log(data)
+    })
+      .catch((error) => {
+      console.log(error)
+    })
+  
+    }; displayData()
+  }, []);
+
+
   return (
-    <UseFetch />
+    <div>
+      Bankcard component
+    </div>
   );
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,20 +5,27 @@ import Bankcard from "./Bankcard";
 
 import { supabase } from "../supabaseClient";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { SignIn } from "@supabase/auth-ui-react";
 
 function Profile() {
   const [userDisplayName, setUserDisplayName] = useState('')
 
   useEffect(() => {
     const displayUserName = async () => {
-      const { data: { user }, error } = await supabase.auth.getUser()
-      setUserDisplayName(user.user_metadata.full_name)
+      const { data: {user}, error } = await supabase.auth.getUser()
+      if (user){
+        setUserDisplayName(user.user_metadata.full_name)
+      }
+      else {
+        setUserDisplayName('')
+      }
       if (error){
         console.log(error)
       }
     }; displayUserName()
-  }, []);
-
+  }, [userDisplayName]);
+  
   return (
     <div className="h-screen">
 
@@ -29,7 +36,7 @@ function Profile() {
 
         <div className="p-20">
           <div>
-            <p>{userDisplayName ? `Hi, ${userDisplayName}!`: ''}</p>
+            <p>{userDisplayName ? `Hi, ${userDisplayName}!`: <Link to={'/login'}>Click Here to Sign In</Link>}</p>
           </div>
           <Bankcard />
         </div>
