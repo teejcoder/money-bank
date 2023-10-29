cbf98277316a215fd6bce1f179d3cf4163d49d4a
Keej <96551689+teejcoder@users.noreply.github.com>
2023-10-29 12:27:44 +1100

    latest changes

--- src/components/Bankcard.jsx
+++ src/components/Bankcard.jsx
@@ -1,28 +1,23 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 
- 
 const Bankcard = () => {
   const api_key = process.env.REACT_APP_BASIQ_API_KEY;
   
-  useEffect(() => {
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'basiq-version': '3.0',
-        'content-type': 'application/x-www-form-urlencoded',
-        Authorization: 'Basic MzA4NDMyZTAtNjgzOS00ZGU0LWJkZDEtOWVhMzc2ZWUyZDJhOmNhNzhiODhmLWEyZTgtNDZjOS1iNWZhLTcyZDdjNGI4YTMxNA=='
-      }
-    };
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'basiq-version': '3.0',
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic MzA4NDMyZTAtNjgzOS00ZGU0LWJkZDEtOWVhMzc2ZWUyZDJhOmNhNzhiODhmLWEyZTgtNDZjOS1iNWZhLTcyZDdjNGI4YTMxNA=='
+    }
+  };
     
-    fetch('https://au-api.basiq.io/token', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
-  }, []);
+  fetch('https://au-api.basiq.io/token', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
   
-
   
   return (
     <div className='h-full w-full flex justify-center items-center flex-col'>
