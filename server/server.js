73114e8ab1e7d967880a74bd00a2fd82b50cc905
Keej <96551689+teejcoder@users.noreply.github.com>
2023-12-31 15:08:32 +1100

    JSON data successfully rendered on client localhost

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const app = express();
+
 const cors = require('cors');
 const path = require('path'); 
-const mainRoutes = require('./routes/mainRoutes');
-const apiRoutes = require('./routes/apiRoutes');
-const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
-const app = express();
 const port = process.env.PORT || 5001;
 
 // Use CORS middleware
@@ -14,12 +12,8 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.use("/", mainRoutes)
-app.use("/auth", authRoutes);
-
-
-app.get('/', (req, res) => {
-  res.render(path.join(__dirname, 'client/src', 'App.js'));
+app.get("/api", (req, res) => {
+  res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
 });  
 
 // Start the server
