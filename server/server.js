eb4e3a89c47266e6c4c0a5e46ebc1e2929900dc9
Keej <96551689+teejcoder@users.noreply.github.com>
2024-01-27 13:12:33 +1100

    server.js edited to find build folder

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('/api', apiRoutes);
 app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // Start the server
