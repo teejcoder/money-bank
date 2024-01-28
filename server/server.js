c4cd0528d94f4500f72a5953c829c24d3d082d88
Keej <96551689+teejcoder@users.noreply.github.com>
2024-01-28 18:32:04 +1100

    latest

--- server/server.js
+++ server/server.js
@@ -17,13 +17,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.use('/api', apiRoutes);
 app.use('/auth', authRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
