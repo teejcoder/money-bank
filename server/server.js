1a43c95384a98b17a915a20f385476dadfa3c9c6
Keej <96551689+teejcoder@users.noreply.github.com>
2024-01-28 22:02:50 +1100

    latest

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use('/api', apiRoutes);
 app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
