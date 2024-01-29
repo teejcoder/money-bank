860eb32737bb57691a8a42da5f737f584ef42b38
Keej <96551689+teejcoder@users.noreply.github.com>
2024-01-29 14:48:37 +1100

    latest

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: 'http://localhost:5001/',
+  origin: 'https://kind-gold-chinchilla-wear.cyclic.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
