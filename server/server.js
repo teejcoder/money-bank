5d4b458e2ccd2b39f922d56c8acdbe911c8d2087
Keej <96551689+teejcoder@users.noreply.github.com>
2024-01-29 18:07:45 +1100

    latest

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: 'https://drab-blue-vulture-garb.cyclic.app',
+  origin: 'http://localhost:5001',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
